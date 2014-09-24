2ea8927eb5ce89099697d68ae841a8dbc4442b94
saullogiovani <saullogiovani@gmail.com>
2014-09-24 11:04:32 +0200

    changed from np.interp to interp_polar

--- desicos/abaqus/imperfections/uneven_edges.py
+++ desicos/abaqus/imperfections/uneven_edges.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from desicos.conecylDB.interpolate import interp_polar
+
 class Shim(object):
     """Represents a shim added to one of the edges
 
@@ -168,15 +170,13 @@ class UnevenBottomEdge(object):
         # contributions from measured edge imperfection
         if self.measured_u3s!=None:
             measured_u3s = np.asarray(self.measured_u3s)
-            measured_u3s[0, :] %= 360
         else:
             measured_u3s = np.zeros((2, 100))
             measured_u3s[0, :] = np.linspace(0, 360, 100)
-        #   sorting to cope with `np.interp` requirements
-        measured_u3s = measured_u3s[:, np.argsort(measured_u3s[0, :])]
         #   calculating u3 for each node
-        u3_nodes = np.interp(np.rad2deg(theta_nodes) % 360,
-                             measured_u3s[0, :], measured_u3s[1, :])
+        u3_nodes = interp_polar(np.rad2deg(theta_nodes), measured_u3s[0, :],
+                measured_u3s[1, :], degrees=True)
+
         # contributions from shims
         hs = np.zeros_like(theta_nodes)
         for s in self.shims:
@@ -388,15 +388,12 @@ class UnevenTopEdge(object):
         # contributions from measured edge imperfection
         if self.measured_u3s!=None:
             measured_u3s = np.asarray(self.measured_u3s)
-            measured_u3s[0, :] %= 360
         else:
             measured_u3s = np.zeros((2, 100))
             measured_u3s[0, :] = np.linspace(0, 360, 100)
-        #   sorting to cope with `np.interp` requirements
-        measured_u3s = measured_u3s[:, np.argsort(measured_u3s[0,:])]
         #   calculating u3 for each node
-        u3_nodes = np.interp(np.rad2deg(theta_nodes) % 360,
-                             measured_u3s[0, :], measured_u3s[1, :])
+        u3_nodes = interp_polar(np.rad2deg(theta_nodes), measured_u3s[0, :],
+                measured_u3s[1, :], degrees=True)
         #   applying load asymmetry according to cc.betarad and omega
         betarad = np.deg2rad(cc.betadeg)
         omegarad = np.deg2rad(cc.omegadeg)
