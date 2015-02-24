8a2ac274a90f0e2354857c13457517691b37c018
Jasper Reichardt <Jasper.Reichardt@dlr.de>
2015-02-24 16:26:27 +0100

    MAINT: Move all the plotting code from conecyl.py to _plot.py.

--- desicos/abaqus/conecyl/conecyl.py
+++ desicos/abaqus/conecyl/conecyl.py
@@ -1,5 +1,4 @@
 import os
-from os.path import basename
 import traceback
 import multiprocessing
 
@@ -821,131 +820,8 @@ class ConeCyl(object):
             values. The order of the arrays is arbitrary.
 
         """
-        from abaqus import mdb
-        from abaqusConstants import NODAL
-        import desicos.abaqus.abaqus_functions as abaqus_functions
-
-        if self.model_name in mdb.models.keys():
-            part = mdb.models[self.model_name].parts[self.part_name_shell]
-        else:
-            text = 'The model corresponding to the active odb must be loaded'
-            raise RuntimeError(text)
-
-        elements = part.elements
-        nodes = part.nodes
-        sina = np.sin(self.alpharad)
-        cosa = np.cos(self.alpharad)
-
-        odb = abaqus_functions.get_current_odb()
-        odbDisplay = abaqus_functions.get_current_odbdisplay()
-        frame = abaqus_functions.get_current_frame()
-        fieldOutputKey = odbDisplay.primaryVariable[0]
-        sub_vector = odbDisplay.primaryVariable[5]
-
-        frame_num = int(frame.frameValue)
-        field = frame.fieldOutputs[fieldOutputKey]
-        nodal_out = False
-        if field.values[0].position == NODAL:
-            nodal_out = True
-
-        if nodal_out:
-            odbSet = odb.rootAssembly.nodeSets['SHELL_FACES']
-            coords = np.array([n.coordinates for n in nodes])
-            labels = np.array([n.label for n in nodes])
-            if ignore:
-                mask = np.in1d(labels, ignore)
-                labels = labels[mask]
-                coords = coords[mask]
-            thetas = np.arctan2(coords[:, 1], coords[:, 0])
-        else:
-            odbSet = odb.rootAssembly.elementSets['SHELL_FACES']
-            coords = utils.vec_calc_elem_cg(elements)
-            thetas = np.arctan2(coords[:, 1], coords[:, 0])
-
-        field = field.getSubset(region=odbSet)
-
-        attributes = {
-                      'Magnitude': 'magnitude',
-                      'Mises': 'mises',
-                      'Min. In-Plane Principal': 'minInPlanePrincipal',
-                      'Max. In-Plane Principal': 'maxInPlanePrincipal',
-                      'Min. Principal': 'minPrincipal',
-                      'Mid. Principal': 'midPrincipal',
-                      'Max. Principal': 'maxPrincipal',
-                      'Out-of-Plane Principal': 'outOfPlanePrincipal',
-                      'Tresca': 'tresca',
-                      'Third Invariant': 'inv3',
-                     }
-        components = {
-                      'S11': 0,
-                      'S22': 1,
-                      'S33': 2,
-                      'S12': 3,
-                      'SF1': 0,
-                      'SF2': 1,
-                      'SF3': 3,
-                      'SM2': 0,
-                      'SM1': 1,
-                      'SM3': 2,
-                     }
-
-        if sub_vector == '':
-            out = np.array([val.data for val in field.values])
-
-        elif sub_vector in attributes.keys():
-            attr = attributes.get(sub_vector)
-            out = np.array([getattr(v, attr) for v in field.values])
-
-        elif sub_vector in components.keys():
-            pos = components[sub_vector]
-            out = np.array([v.data[pos] for v in field.values])
-
-        elif sub_vector in ('U1', 'UT1', 'U2', 'UT2', 'U3', 'UT3'):
-            uvw_rec = np.array([val.data for val in field.values])
-
-            u1_rec = uvw_rec[:,0]
-            u2_rec = uvw_rec[:,1]
-            u3_rec = uvw_rec[:,2]
-
-            u3_cyl = u3_rec
-            u2_cyl = u2_rec*np.cos(thetas) - u1_rec*np.sin(thetas)
-            u1_cyl = u2_rec*np.sin(thetas) + u1_rec*np.cos(thetas)
-
-            u1_cone = u1_cyl*cosa + u3_cyl*sina
-            u2_cone = u2_cyl
-            u3_cone = u3_cyl*cosa - u1_cyl*sina
-
-            displ_vecs = {'U1':u1_cone, 'U2':u2_cone, 'U3':u3_cone,
-                          'UT1':u1_cone, 'UT2':u2_cone, 'UT3':u3_cone}
-            out = displ_vecs[sub_vector]
-
-        else:
-            raise NotImplementedError('{0} cannot be used!'.format(
-                sub_vector))
-
-        if not nodal_out:
-            firstElement = None
-            numIntPts = 0
-            for v in field.values:
-                if firstElement is None:
-                    firstElement = v.elementLabel
-                if v.elementLabel == firstElement:
-                    numIntPts += 1
-                else:
-                    break
-            out = out.reshape(-1, numIntPts).mean(axis=1)
-
-        if 'S8' in self.elem_type and nodal_out:
-            center_nodes = []
-            for el in elements:
-                center_nodes += el.connectivity[4:]
-            mask = np.in1d(labels, center_nodes)
-            thetas = thetas[mask]
-            coords = coords[mask]
-            out = out[mask]
-
-        zs = coords[:, 2]
-        return (thetas, zs, out)
+        import _plot
+        return _plot.extract_field_output(self, ignore)
 
 
     def transform_raw_plot_data(self, thetas, zs, values, plot_type):
@@ -977,62 +853,8 @@ class ConeCyl(object):
             field output.
 
         """
-        sina = np.sin(self.alpharad)
-        cosa = np.cos(self.alpharad)
-
-        valid_plot_types = (1, 2, 3, 4, 5)
-        if not plot_type in valid_plot_types:
-            raise ValueError('Valid values for plot_type are:\n\t\t' +
-                             ' or '.join(map(str, valid_plot_types)))
-
-        #first sort
-        nt = self.numel_r
-        asort = zs.argsort()
-        zs = zs[asort].reshape(-1, nt)
-        thetas = thetas[asort].reshape(-1, nt)
-        values = values[asort].reshape(-1, nt)
-
-        #second sort
-        asort = thetas.argsort(axis=1)
-        for i, asorti in enumerate(asort):
-            zs[i,:] = zs[i,:][asorti]
-            thetas[i,:] = thetas[i,:][asorti]
-            values[i,:] = values[i,:][asorti]
-
-        H = self.H
-        rtop = self.rtop
-        rbot = self.rbot
-        L = H/cosa
-
-        def fr(z):
-            return rbot - z*sina/cosa
-
-        if self.alpharad == 0.:
-            plot_type = 4
-        if plot_type == 1:
-            r_plot = fr(zs)
-            if self.alpharad==0.:
-                r_plot_max = L
-            else:
-                r_plot_max = rtop/sina + L
-            y = r_plot_max - r_plot*np.cos(thetas*sina)
-            x = r_plot*np.sin(thetas*sina)
-        elif plot_type == 2:
-            r_plot = fr(zs)
-            y = r_plot*np.cos(thetas*sina)
-            x = r_plot*np.sin(thetas*sina)
-        elif plot_type == 3:
-            r_plot = fr(zs)
-            r_plot_max = rtop/sina + L
-            y = r_plot_max - r_plot*np.cos(thetas)
-            x = r_plot*np.sin(thetas)
-        elif plot_type == 4:
-            x = fr(zs)*thetas
-            y = zs
-        elif plot_type == 5:
-            x = fr(0)*thetas
-            y = zs
-        return x, y, values
+        import _plot
+        return _plot.transform_raw_plot_data(self, thetas, zs, values, plot_type)
 
 
     def plot_field_data(self, x, y, field, create_npz_only=False, ax=None,
@@ -1084,101 +906,11 @@ class ConeCyl(object):
         importable from Abaqus.
 
         """
-        npzname = npzname.split('.npz')[0] + '.npz'
-        pyname = pyname.split('.py')[0] + '.py'
-        pngname = pngname.split('.png')[0] + '.png'
-
-        npzname = os.path.join(outpath, npzname)
-        pyname = os.path.join(outpath, pyname)
-        pngname = os.path.join(outpath, pngname)
-
-        if not create_npz_only:
-            try:
-                import matplotlib.pyplot as plt
-                import matplotlib
-            except:
-                create_npz_only = True
-
-        if not create_npz_only:
-            levels = np.linspace(field.min(), field.max(), num_levels)
-            if ax is None:
-                fig = plt.figure(figsize=figsize)
-                ax = fig.add_subplot(111)
-            else:
-                if isinstance(ax, matplotlib.axes.Axes):
-                    ax = ax
-                    fig = ax.figure
-                    save_png = False
-                else:
-                    raise ValueError('"ax" must be an Axes object')
-            ax.contourf(x, y, field, levels=levels)
-            ax.grid(False)
-            ax.set_aspect(aspect)
-            ax.xaxis.set_ticks_position('bottom')
-            ax.yaxis.set_ticks_position('left')
-            #lim = self.rtop*np.pi
-            #ax.xaxis.set_ticks([-lim, 0, lim])
-            #ax.xaxis.set_ticklabels([r'$-\pi$', '$0$', r'$+\pi$'])
-            #ax.set_title(
-                #r'$PL=20 N$, $F_{{C}}=50 kN$, $w_{{PL}}=\beta$, $mm$')
-            if clean:
-                ax.xaxis.set_ticks_position('none')
-                ax.yaxis.set_ticks_position('none')
-                ax.xaxis.set_ticklabels([])
-                ax.yaxis.set_ticklabels([])
-                ax.set_frame_on(False)
-            if save_png:
-                log('')
-                log('Plot saved at: {0}'.format(pngname))
-                plt.tight_layout()
-                plt.savefig(pngname, transparent=True,
-                            bbox_inches='tight', pad_inches=0.05,
-                            dpi=400)
-
-        else:
-            log('Matplotlib cannot be imported from Abaqus')
-        np.savez(npzname, x=x, y=y, field=field)
-        with open(pyname, 'w') as f:
-            f.write("import os\n")
-            f.write("\n")
-            f.write("import numpy as np\n")
-            f.write("import matplotlib.pyplot as plt\n")
-            f.write("\n")
-            f.write("add_title = False\n")
-            f.write("tmp = np.load(r'{0}')\n".format(basename(npzname)))
-            f.write("pngname = r'{0}'\n".format(basename(pngname)))
-            f.write("x = tmp['x']\n")
-            f.write("y = tmp['y']\n")
-            f.write("field = tmp['field']\n")
-            f.write("clean = {0}\n".format(clean))
-            f.write("plt.figure(figsize={0})\n".format(figsize))
-            f.write("ax = plt.gca()\n")
-            f.write("levels = np.linspace(field.min(), field.max(), {0})\n".format(
-                    num_levels))
-            f.write("ax.contourf(x, y, field, levels=levels)\n")
-            f.write("ax.grid(False)\n")
-            f.write("ax.set_aspect('{0}')\n".format(aspect))
-            f.write("ax.xaxis.set_ticks_position('bottom')\n")
-            f.write("ax.yaxis.set_ticks_position('left')\n")
-            f.write("ax.xaxis.set_ticks([{0}, 0, {1}])\n".format(
-                    -self.rtop*np.pi, self.rtop*np.pi))
-            f.write("ax.xaxis.set_ticklabels([r'$-\pi$', '$0$', r'$+\pi$'])\n")
-            f.write("if add_title:\n")
-            f.write("    ax.set_title(r'Abaqus, $PL=20 N$, $F_{{C}}=50 kN$, $w_{{PL}}=\beta$, $mm$')\n")
-            f.write("if clean:\n")
-            f.write("    ax.xaxis.set_ticks_position('none')\n")
-            f.write("    ax.yaxis.set_ticks_position('none')\n")
-            f.write("    ax.xaxis.set_ticklabels([])\n")
-            f.write("    ax.yaxis.set_ticklabels([])\n")
-            f.write("    ax.set_frame_on(False)\n")
-            f.write("plt.savefig(pngname, transparent=True,\n")
-            f.write("            bbox_inches='tight', pad_inches=0.05, dpi=400)\n")
-            f.write("plt.show()\n")
-        log('')
-        log('Output exported to "{0}"'.format(npzname))
-        log('Please run the file "{0}" to plot the output'.format(
-              pyname))
-        log('')
+        # avoid retyping all arguments, while keeping the function arguments
+        # and default values clearly visible here, for documentation purposes
+        kwargs = locals()
+        import _plot
+        return _plot.plot_field_data(**kwargs)
 
 
     def plot_opened(self, ignore=[], plot_type=1, **kwargs):
