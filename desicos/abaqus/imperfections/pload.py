3bdd2023ab77d10720eec2fc2d96d12e9df5d744
saullocastro <saullogiovani@gmail.com>
2015-03-03 21:40:26 -0300

    DEV: added method to calculate imperfection amplitude

--- desicos/abaqus/imperfections/pload.py
+++ desicos/abaqus/imperfections/pload.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import sin, cos
 
 from imperfection import Imperfection
 from desicos.abaqus.constants import *
@@ -26,13 +27,14 @@ class PLoad(Imperfection):
         self.xaxis = 'pltotal'
         self.xaxis_label = 'Perturbation Load, N'
 
+
     def rebuild(self):
         cc = self.impconf.conecyl
         alpharad = cc.alpharad
-        self.plradial = self.pltotal*np.cos(alpharad)
-        self.plz = -self.pltotal*np.sin(alpharad)
-        self.plx = -self.plradial*np.cos(np.deg2rad(self.thetadeg))
-        self.ply = -self.plradial*np.sin(np.deg2rad(self.thetadeg))
+        self.plradial = self.pltotal*cos(alpharad)
+        self.plz = -self.pltotal*sin(alpharad)
+        self.plx = -self.plradial*cos(np.deg2rad(self.thetadeg))
+        self.ply = -self.plradial*sin(np.deg2rad(self.thetadeg))
         self.x, self.y, self.z = self.get_xyz()
         self.r, z = cc.r_z_from_pt(self.pt)
         self.thetadeg = self.thetadeg % 360.
@@ -43,11 +45,46 @@ class PLoad(Imperfection):
         if abs(self.pltotal) < 0.1*TOL:
             warn('Ignoring perturbation load: {0}'.format(self.name))
 
+
     def calc_amplitude(self):
-        return self.pltotal
+        """Calculate the imperfection amplitude.
+
+        The odb must be available and it will be used to extract the last
+        frame of the first analysis step, corresponding to the constant loads.
+
+        """
+        from abaqus import mdb
+
+        cc = self.impconf.conecyl
+        mod = mdb.models[cc.model_name]
+        nodes = mod.parts[cc.part_name_shell].nodes
+        xmin = self.x-1.e-3
+        xmax = self.x+1.e-3
+        ymin = self.y-1.e-3
+        ymax = self.y+1.e-3
+        zmin = self.z-1.e-3
+        zmax = self.z+1.e-3
+        node = nodes.getByBoundingBox(xmin, ymin, zmin, xmax, ymax, zmax)[0]
+
+        odb = cc.attach_results()
+        fo = odb.steps[cc.step1Name].frames[-1].fieldOutputs
+        if not 'U' in fo.keys():
+            raise RuntimeError(
+                    'Field output 'U' not available to calculate amplitude')
+        #TODO not sure if this is robust: node.label-1
+        u, v, w = fo['U'].values[node.label-1].data
+        cc.detach_results(odb)
+
+        alpha = cc.alpharad
+        theta = np.deg2rad(self.thetadeg)
+        amp = -((u*cos(theta) + v*sin(theta))*cos(alpha) + w*sin(alpha))
+        self.amplitude = amp
+
+        return amp
+
 
     def create(self):
-        """Includes the perturbation load
+        """Include the perturbation load.
 
         The load step in which the perturbation load is included depends on
         the ``step`` parameter, which can be 1 or 2. If applied in the first
