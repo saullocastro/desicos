cd75727d91a8f00dc85b737aea7e2c0cedf7f110
Jasper Reichardt <Jasper.Reichardt@dlr.de>
2015-04-20 11:31:05 +0200

    DEV: Add a collection of functions and classes to help with 2D geometrical operations.

---
+++ desicos/cppot/core/__init__.py
@@ -0,0 +1,8 @@
+r"""
+===================================================
+Cone Ply Piece Optimization Tool (:mod:`desicos.cppot`)
+===================================================
+
+.. currentmodule:: desicos.cppot
+
+"""
