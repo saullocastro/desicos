b507891034a5310c968016670589c7994285c9f9
saullocastro <saullogiovani@gmail.com>
2015-04-30 21:14:35 -0300

    DOC: improved documentation

--- desicos/cppot/core/__init__.py
+++ desicos/cppot/core/__init__.py
@@ -1,8 +1,15 @@
 r"""
-===================================================
-Cone Ply Piece Optimization Tool (:mod:`desicos.cppot`)
-===================================================
+============================================================
+Cone Ply Piece Optimization Tool (:mod:`desicos.cppot.core`)
+============================================================
 
-.. currentmodule:: desicos.cppot
+.. currentmodule: desicos.cppot.core
+
+.. automodule:: desicos.cppot.core.geom
+    :members:
+
+.. automodule:: desicos.cppot.core.ply_model
+    :members:
 
 """
+pass
--- desicos/cppot/gui/__init__.py
+++ desicos/cppot/gui/__init__.py
@@ -5,4 +5,8 @@ CPPOT GUI (:mod:`desicos.cppot.gui`)
 
 .. currentmodule:: desicos.cppot.gui
 
+All the functionalities of the Graphic User Interface will be started by
+clicking on the ``Click_me.pyw`` file.
+
 """
+pass
